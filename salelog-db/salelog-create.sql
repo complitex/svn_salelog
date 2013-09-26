1c7e5b0aab2e0b3c1701a2f9d962caa1bd37e9fe
pavel.sknar <pavel.sknar@gmail.com>
2013-09-26 10:13:42 +0000

    Price and total cost of product sale

--- salelog-db/salelog-create.sql
+++ salelog-db/salelog-create.sql
@@ -69,6 +69,8 @@ CREATE TABLE `product_sale` (
   `pk_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `order_id` BIGINT(20) NOT NULL,
   `product_code` VARCHAR(255) NOT NULL,
+  `price` decimal(19,2),
+  `total_cost` decimal(19,2),
   `begin_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `end_date` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY  (`pk_id`),
